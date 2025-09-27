8324f208b740fed6d5b9b3a0f740260b10cc8fe2
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2025-09-27 20:11:23 +0000

    init setup

---
+++ src/main/java/com/backend/susu_box/SusuBoxApplication.java
@@ -0,0 +1,13 @@
+package com.backend.susu_box;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusuBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SusuBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/susu_box/SusuBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.susu_box;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SusuBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
