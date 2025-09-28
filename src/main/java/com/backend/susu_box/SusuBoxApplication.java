245b2a02316b219c5a83f6268ab6ad3e2fea2abf
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2025-09-28 18:04:59 +0000

    feat(config): add dev profile and env vars
    Add development profile and configure environment variables.
    
    - Introduce application-dev.properties for dev settings.
    - Configure datasource and MongoDB connection using env vars.
    - Enable scheduling in SusuBoxApplication.
    
    chore(gitignore): ignore .env file
    
    Add .env to .gitignore to prevent sensitive data commits.

--- src/main/java/com/backend/susu_box/SusuBoxApplication.java
+++ src/main/java/com/backend/susu_box/SusuBoxApplication.java
@@ -2,8 +2,10 @@ package com.backend.susu_box;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SusuBoxApplication {
 
 	public static void main(String[] args) {
